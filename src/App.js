6a47092be695ee425d41526e30685227e8104bee
Cam <cameron.dyas98@gmail.com>
2022-08-21 03:14:38 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About"
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+import React, { useState,useEffect } from 'react';
+// import ClipLoader from "react-spinners/ClipLoader";
+import Loader from "./components/Loader";
+
+
 
 function App() {
+  const [ loading,setLoading ] = useState(false);
+
+  
+  useEffect(()=>{
+    // var element = document.querySelector(".app")
+    // element.classList.add("loader")
+    setLoading(true)
+    setTimeout(() =>{
+      // element.classList.remove("loader")
+      setLoading(false)
+    },3000)
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {loading ? <Loader /> : 
+       <><Navbar /><Home /><About /><Skills /><Work /><Contact /></> }
     </div>
   );
 }
---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React, {useState,useEffect} from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
+import rabbit from "../assets/rabbitrun.gif"
+const Loader = () => {
+  return (
+      <div className = 'max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full opacity-[0.9] transition-all duration-300 ease-linear'>
+        <img src={rabbit} alt="loading..." />
+      </div>
+  )
+}
+
+export default Loader
